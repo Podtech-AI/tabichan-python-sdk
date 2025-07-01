ba0ce390dbba12895a19d11b4eff527a03f0fe6e
maxenceleguery <maxence.leguery@gmail.com>
2025-07-01 15:04:48 +0000

    :bookmark: Adding script

---
+++ scripts/chat.py
@@ -0,0 +1,15 @@
+from tabichan.client import TabichanClient
+
+client = TabichanClient()
+
+task_id = client.start_chat(
+    user_query="Plan a 2-day trip to Tokyo for this weekend",
+    user_id="user123",
+    country="japan",
+)
+
+print(task_id)
+
+result = client.wait_for_chat(task_id)
+
+print(result)
